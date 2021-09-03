f3fef3a0ec0cccc63cd8729adf071acdd49de2ad
Harsh Chauhan <charsh458@gmail.com>
2021-09-03 18:16:25 +0530

    Initial commit

---
+++ src/brickBreaker/gamePlay.java
@@ -0,0 +1,202 @@
+package brickBreaker;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.Timer;
+
+import javax.swing.JPanel;
+
+public class gamePlay extends JPanel implements KeyListener, ActionListener{
+	
+	private boolean play=false;
+	private int score =0;
+	private int totalBricks=21;
+	
+	private Timer timer; 
+	private int delay=-30;
+	
+	private int playerX=310;
+	
+	private int ballposX=120;
+	private int ballposY=350;
+	private int balldirX=-1;
+	private int balldirY=-2;
+	
+	private MapGenerator map;
+	
+	public gamePlay() {
+		map=new MapGenerator(3,7);
+		addKeyListener(this);
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(false);
+		timer=new Timer(delay,this);
+		timer.start();
+	}
+	
+	public void paint(Graphics g) {
+		//background
+		g.setColor(Color.black);
+		g.fillRect(1, 1, 692, 592);
+		
+		//display score
+		g.setColor(Color.white);
+		g.setFont(new Font("serif",Font.BOLD,25));
+		g.drawString("Score:"+score, 550, 30);
+		//draw bricks
+		map.draw((Graphics2D)g);
+		
+		//border
+		g.setColor(Color.yellow);
+		g.fillRect(0, 0, 3, 592);
+		g.fillRect(0, 0, 692, 3);
+		g.fillRect(691, 0, 3, 592);
+		
+		//paddle
+		g.setColor(Color.green);
+		g.fillRect(playerX, 550, 100, 8);
+		
+		//ball
+		g.setColor(Color.pink);
+		g.fillOval(ballposX, ballposY, 20, 20);
+		
+		if(totalBricks<=0) {
+			play=false;
+			balldirX=0;
+			balldirY=0;
+			
+			g.setColor(Color.RED);
+			g.setFont(new Font("serif",Font.BOLD,30));
+			g.drawString("Hurray!You Won",210,320);
+			
+			g.setColor(Color.RED);
+			g.setFont(new Font("serif",Font.BOLD,25));
+			g.drawString("Press enter to restart", 210,320);
+		}
+		
+		if(ballposY>570) {
+			play=false;
+			balldirX=0;
+			balldirY=0;
+			
+			g.setColor(Color.RED);
+			g.setFont(new Font("serif",Font.BOLD,30));
+			g.drawString("Game Over", 210,280);
+			
+			g.setColor(Color.RED);
+			g.setFont(new Font("serif",Font.BOLD,25));
+			g.drawString("Press enter to restart", 210,320);
+		}
+		
+		g.dispose();
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		timer.start();
+		
+		if(play) {
+			if(new Rectangle(ballposX,ballposY, 20, 20).intersects(new Rectangle(playerX,550, 100,8))) {
+				balldirY=-balldirY;
+			}
+			A:for(int i=0;i<map.map.length;i++) {
+				for(int j=0;j<map.map[0].length;j++) {
+					if(map.map[i][j]>0) {
+						int brickX=j*map.brickWidth+80;
+						int brickY=i*map.brickHeight+50;
+						int brickHeight=map.brickHeight;
+						int brickWidth=map.brickWidth;
+						
+						Rectangle rect=new Rectangle(brickX, brickY, brickWidth, brickHeight);
+						Rectangle ballRect=new Rectangle(ballposX,ballposY,20,20);
+						Rectangle brickRect=rect;
+						
+						if(brickRect.intersects(ballRect)) {
+							map.setBrickValue(0,i,j);
+							totalBricks--;
+							score+=5;
+							
+							if(ballposX+ 19<= brickRect.x || ballposX+ 1>= brickRect.x+ brickRect.width) {
+								balldirX=-balldirX;
+							}
+							else
+								balldirY=-balldirY;
+							break A;
+						}
+					}
+				}
+			}
+			
+			ballposX+=balldirX;
+			ballposY+=balldirY;
+			if(ballposX<0) {
+				balldirX=-balldirX;
+			}
+			if(ballposY<0) {
+				balldirY=-balldirY;
+			}
+			if(ballposX>670) {
+				balldirX=-balldirX;
+			}
+		}
+		repaint();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			if(playerX>=600)
+				playerX=600;
+			else
+				moveRight();
+		}
+		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+			if(playerX<=10)
+				playerX=10;
+			else
+				moveLeft();
+		}
+		
+		if(e.getKeyCode()==KeyEvent.VK_ENTER) {
+			if(!play) {
+				play=true;
+				ballposX=120;
+				ballposY=350;
+				balldirX=-1;
+				balldirY=-2;
+				playerX=310;
+				score=0;
+				totalBricks=21;
+				map=new MapGenerator(3,7);
+				
+				repaint();
+			}
+		}
+		
+	}
+	public void moveRight() {
+		play=true;
+		playerX+=20;
+	}
+	public void moveLeft() {
+		play=true;
+		playerX-=20;
+	}
+	
+	
+}
